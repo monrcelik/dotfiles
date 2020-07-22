4efb5aab25677f2e38b5ed33043badb35e5b83a9
onur <mail@onrc.pw>
2020-07-22 19:50:01 +0300

    update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,11 @@
 set fish_greeting
 
 set -x PATH $HOME/.cargo/bin $PATH
+set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore-vcs --hidden'
 
-starship init fish | source
 
 source ~/.config/fish/gnupg.fish
+
+rbenv init - | source
+
+starship init fish | source
