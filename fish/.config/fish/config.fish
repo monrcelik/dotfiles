49968d15810299fc72ada1c9eac998e34fd9c9a5
onur <mail@onrc.pw>
2021-02-19 22:10:13 +0300

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,7 @@
 set fish_greeting
 
+set -x PATH /usr/local/bin $PATH
+set -x PATH /usr/local/sbin $PATH
 set -x PATH $HOME/.cargo/bin $PATH
 set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git"'
 
